b4375716534c5ea5469ae8a67a61cb8d80fcbd2a
Abelsileshie <silesheiabel@gmail.com>
2025-02-05 20:29:14 +0300

    fiexed the bug hapend earlier

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -3,6 +3,7 @@ import App from "./App";
 import VerifyEmail from "./layouts/Shared/Signup/VerifyEmail";
 import Login from "./layouts/Shared/Login/Login";
 import { Route, Routes } from "react-router-dom";
+import Signup from "./layouts/Shared/Signup/Signup";
 
 const routes = () => {
   return (
@@ -11,6 +12,7 @@ const routes = () => {
         <Route path="/" element={<App />} />
         <Route path="/verifyEmail" element={<VerifyEmail />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/Signup" element={<Signup />} />
         <Route path="*" element={<h1>Not Found</h1>} />
       </Routes>
     </>
